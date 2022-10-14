bbfab51d95a650061cebfb8a2f6562ad48b18e02
Minamin1234 <minamin37345@gmail.com>
2022-10-14 07:37:32 +0000

    修正

--- 10_07/09.cpp
+++ 10_07/09.cpp
@@ -6,11 +6,16 @@ template<class T,int N>
 int size(T (&ar)[N]) { return N;}
 
 template<class T>
-int size(const T& container) { return container.size(); }
+int size(const T& container) { return container->size(); }
 
 int main()
 {
     int x[10];
     std::cout << size(x) << std::endl;
+    /*
+    コンテナの方
+    std::vector<int> v = {1,2,3,4,5};
+    std::cout << size(&v) << std::endl;
+    */
     return 0;
 }
