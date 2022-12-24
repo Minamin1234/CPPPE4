0aa48150e1b1b0ea8ba5b09b02e254f23b6e3ae8
Minamin1234 <minamin37345@gmail.com>
2022-12-24 20:41:22 +0900

    コメントの追加

--- 12_2/ranking.cpp
+++ 12_2/ranking.cpp
@@ -5,24 +5,35 @@
 #include <algorithm>
 
 int main(int argc,char* argv[]) {
-    std::ifstream ifs(argv[1]);
-    std::string word;
-    std::map<char, int> cnts;
-    std::vector<std::pair<int, char>> ranks;    
+    std::ifstream ifs(argv[1]);  // ファイル読み込み
+    std::string word;  // 単語を格納する変数
+    std::map<char, int> cnts;  // 文字ごとの出現数を格納する連想配列
+    std::vector<std::pair<int, char>> ranks;  // (出現数, 文字)のペア配列
 
+    // a~zをそれぞれ0として格納する
     for(char c = 'a';c <= 'z';c++) {
         cnts[c] = 0;
     }
+
+    // ファイルから単語単位で取得する
     while(ifs >> word) {
         for(auto s : word) {
-            if(cnts.count(s)) cnts[s]++;
+            if(cnts.count(std::tolower(s))) cnts[s]++;  // 存在する文字であれば
         }
-    } 
+    }
+
+    // 文字と出現数を反転させてペア配列に格納する
     for(const auto &s : cnts) {
         ranks.push_back(std::make_pair(s.second,s.first));
     }
+
+    // 出現数でソート
     std::sort(ranks.begin(),ranks.end());
+
+    // 昇順から降順に
     std::reverse(ranks.begin(),ranks.end());
+
+    // 出力
     for(auto p : ranks) {
         std::cout << p.second << ": " << p.first << std::endl;
     }
